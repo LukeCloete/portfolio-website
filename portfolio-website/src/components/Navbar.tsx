1cb03c01f8e7bb6a8d98a29417f574233fe9b204
Luke Cloete <Cloeteluke@gmail.com>
2023-10-17 23:28:54 +0200

    Style fix

--- portfolio-website/src/components/Navbar.tsx
+++ portfolio-website/src/components/Navbar.tsx
@@ -77,7 +77,7 @@ function Nav() {
               <button>
                 <Link
                   href=""
-                  className=" before:ease relative h-12 w-40 overflow-hidden shadow-2xl transition-all before:absolute before:right-0 before:top-0 before:h-12 before:w-6 before:translate-x-12 before:rotate-6 before:bg-white before:opacity-10 before:duration-700 hover:shadow-blue-500 hover:before:-translate-x-40 rounded-sm p-3 px-5 text-sm font-bold bg-blue-500 text-white"
+                  className=" before:ease relative h-12 w-40 overflow-hidden shadow-2xl transition-all before:absolute before:right-0 before:top-0 before:h-12 before:w-6 before:translate-x-12 before:rotate-6 before:bg-white before:opacity-10 before:duration-700 hover:shadow-blue-500 hover:before:-translate-x-40 rounded-md p-3 px-5 text-sm font-bold bg-blue-500 text-white"
                 >
                   Download my CV
                 </Link>
