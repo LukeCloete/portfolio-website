f513e6a9fa16de24a7b0353b72a9caa2b308c39b
Luke Cloete <Cloeteluke@gmail.com>
2023-10-19 23:56:39 +0200

    Add responsive design

--- portfolio-website/src/components/Hero.tsx
+++ portfolio-website/src/components/Hero.tsx
@@ -13,21 +13,24 @@ import {
   faJava,
 } from "@fortawesome/free-brands-svg-icons";
 import Link from "next/link";
+import tailwindIcon from "../../public/tailwindcss.svg";
+import nextjsIcon from "../../public/next-js.svg";
+import typescriptIcon from "../../public/typescript.svg";
 
 export default function Hero() {
   return (
     <>
       <section id="home" className={styles.section}>
         {/* Container */}
-        <div className="w-full h-full flex flex-col gap-12 justify-center items-center">
+        <div className="w-[80%] md:w-[70%] mx-auto border-4 h-full mt-8 flex flex-col gap-12 lg:gap-24 justify-center items-center">
           {/* Info and Picture */}
-          <div className="w-[70%] flex gap-8 h-3/4">
+          <div className=" w-full flex justify-end md:gap-20 h-3/4 lg:h-1/2 flex-col-reverse md:flex-row">
             {/* Info */}
-            <div className="flex w-full h-full flex-col items-start justify-center">
-              <h1 className=" font-bold h-1/3 leading-tight text-[55px] tracking-tight w-full">
+            <div className="flex w-full h-fit md:h-full flex-col items-center md:items-start justify-start md:justify-center">
+              <h1 className="text-center md:text-left font-bold h-fit py-4 leading-tight text-5xl sm:text-[55px] tracking-tight w-full flex-shrink-0">
                 The Front-end Engineer.
               </h1>
-              <p className="text-lg tracking-relaxed mb-8">
+              <p className=" text-center lg:text-left  text-lg sm:text-lg tracking-relaxed mb-8 flex-shrink-0">
                 Hi, I&apos;m Luke Cloete, a passionate Front-end Software
                 Developer based in Windhoek, Namibia.
               </p>
@@ -56,84 +59,78 @@ export default function Hero() {
                   </Link>
                 </div>
                 <div className="flex items-center justify-center">
-                  <button className=" text-base p-2 rounded-sm transition font-semibold ease-in-out duration-300 bg-black text-white hover:bg-transparent hover:border-black hover:border hover:text-black">
+                  <button className=" text-base p-2 rounded-md transition font-semibold ease-in-out duration-300 bg-black text-white hover:bg-transparent hover:border-black hover:border hover:text-black">
                     Download my CV
                   </button>
                 </div>
               </div>
             </div>
             {/* Picture */}
-            <div className="border border-red-400 w-full h-full">Picture</div>
+            <div className="border border-red-400 lg:h-full w-full h-[65%]">
+              Picture
+            </div>
           </div>
           {/* Tech Stack */}
-          <div className="h-[5vh] flex items-center justify-center w-[70%] flex-row">
-            <div className="w-1/6 text-lg opacity-80">My Tech Stack |</div>
-            <div className="flex items-center w-full h-full">
-              <ul className="flex items-center justify-center gap-4">
-                <li className="flex items-center justify-center gap-2">
-                  <FontAwesomeIcon
-                    icon={faHtml5}
-                    width="28"
-                    height="32"
-                    size="2x"
-                    className="hover:scale-125 ease-in-out transition duration-300"
-                  />
-                  <FontAwesomeIcon
-                    icon={faCss3}
-                    width="32"
-                    height="28"
-                    size="2x"
-                    className="hover:scale-125 ease-in-out transition duration-300"
-                  />
-                </li>
-                <li className="flex items-center justify-center gap-2">
-                  <FontAwesomeIcon
-                    icon={faJava}
-                    width="28"
-                    height="32"
-                    size="2x"
-                    className="hover:scale-125 ease-in-out transition duration-300"
-                  />
-                  <FontAwesomeIcon
-                    icon={faJs}
-                    width="28"
-                    height="32"
-                    size="2x"
-                    className="hover:scale-125 ease-in-out transition duration-300"
-                  />
-                  <Image
-                    height="32"
-                    width="28"
-                    alt="Typescript"
-                    className="hover:scale-125 ease-in-out transition duration-300"
-                    src="./typescript.svg"
-                  />
-                </li>
-                <li className="flex items-center justify-center gap-2">
-                  <FontAwesomeIcon
-                    icon={faReact}
-                    width="28"
-                    height="32"
-                    size="2x"
-                    className="hover:scale-125 ease-in-out transition duration-300"
-                  />
-                  <Image
-                    height="32"
-                    width="28"
-                    alt="Next.js"
-                    className="hover:scale-125 ease-in-out transition duration-300"
-                    src="./next-js.svg"
-                  />
-                  <Image
-                    height="32"
-                    width="28"
-                    alt="Tailwind CSS"
-                    className="hover:scale-125 ease-in-out transition duration-300"
-                    src="./tailwindcss.svg"
-                  />
-                </li>
-              </ul>
+          <div className=" h-fit gap-8 mb-32 lg:mb-8 flex md:flex-row flex-col items-center justify-center md:justify-start w-full">
+            <div className=" w-fit md:w-fit text-center md:text-left font-semibold  border-b-2 border-black md:border-none text-lg opacity-80 md:after:content-['_|']">
+              My Tech Stack
             </div>
+
+            <ul className="flex flex-col md:flex-row items-center justify-center gap-6">
+              <li className="flex items-center justify-center gap-2">
+                <FontAwesomeIcon
+                  icon={faHtml5}
+                  height={50}
+                  size="3x"
+                  className="hover:scale-125 ease-in-out transition duration-300"
+                />
+                <FontAwesomeIcon
+                  icon={faCss3}
+                  height={50}
+                  size="3x"
+                  className="hover:scale-125 ease-in-out transition duration-300"
+                />
+              </li>
+              <li className="flex items-center justify-center gap-2">
+                <FontAwesomeIcon
+                  icon={faJava}
+                  height={50}
+                  size="3x"
+                  className="hover:scale-125 ease-in-out transition duration-300"
+                />
+                <FontAwesomeIcon
+                  icon={faJs}
+                  size="3x"
+                  className="hover:scale-125 ease-in-out transition duration-300"
+                />
+                <Image
+                  height={42}
+                  alt="Typescript"
+                  className="hover:scale-125 ease-in-out transition duration-300"
+                  src={typescriptIcon}
+                />
+              </li>
+              <li className="flex items-center justify-center gap-2">
+                <FontAwesomeIcon
+                  icon={faReact}
+                  height={45}
+                  size="3x"
+                  className="hover:scale-125 ease-in-out transition duration-300"
+                />
+                <Image
+                  height={45}
+                  alt="Next.js"
+                  className="hover:scale-125 ease-in-out transition duration-300"
+                  src={nextjsIcon}
+                />
+                <Image
+                  alt="Tailwind CSS"
+                  height={45}
+                  src={tailwindIcon}
+                  className="hover:scale-125 ease-in-out transition duration-300"
+                />
+              </li>
+            </ul>
           </div>
         </div>
       </section>
