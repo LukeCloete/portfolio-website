8a90c1d0cac350824a291df51faff7f5ab9056b8
Luke Cloete <Cloeteluke@gmail.com>
2023-10-26 00:34:09 +0200

    Add cool blob and other styling changes

--- portfolio-website/src/components/Hero.tsx
+++ portfolio-website/src/components/Hero.tsx
@@ -16,27 +16,28 @@ import Link from "next/link";
 import tailwindIcon from "../../public/tailwindcss.svg";
 import nextjsIcon from "../../public/next-js.svg";
 import typescriptIcon from "../../public/typescript.svg";
+import heroPicture from "../../public/temp-hero-picture.jpg";
 
 export default function Hero() {
   return (
     <>
       <section id="home" className={styles.section}>
         {/* Container */}
-        <div className="w-[80%] md:w-[70%] mx-auto border-4 h-full mt-8 flex flex-col gap-12 lg:gap-24 justify-center items-center">
+        <div className="w-[80%] md:w-[70%] mx-auto h-full  mt-6 md:mt-20 flex flex-col gap-12 lg:gap-24 justify-center items-center">
           {/* Info and Picture */}
           <div className=" w-full flex justify-end md:gap-20 h-3/4 lg:h-1/2 flex-col-reverse md:flex-row">
             {/* Info */}
             <div className="flex w-full h-fit md:h-full flex-col items-center md:items-start justify-start md:justify-center">
-              <h1 className="text-center md:text-left font-bold h-fit py-4 leading-tight text-5xl sm:text-[55px] tracking-tight w-full flex-shrink-0">
+              <h1 className="text-center md:text-left font-bold h-fit py-4 leading-tight text-4xl sm:text-[55px] tracking-tight w-full flex-shrink-0">
                 The Front-end Engineer.
               </h1>
-              <p className=" text-center lg:text-left  text-lg sm:text-lg tracking-relaxed mb-8 flex-shrink-0">
+              <p className=" text-center md:text-left  text-lg sm:text-lg tracking-relaxed mb-8 flex-shrink-0">
                 Hi, I&apos;m Luke Cloete, a passionate Front-end Software
                 Developer based in Windhoek, Namibia.
               </p>
 
-              <div className="flex gap-8">
-                <div className="flex gap-2 items-end">
+              <div className="flex flex-col-reverse sm:flex-row gap-8">
+                <div className="flex gap-2 items-end justify-center md:justify-start">
                   <Link
                     href="https://www.linkedin.com/in/luke-cloete-2b288b231/"
                     target="blank_"
@@ -66,12 +67,17 @@ export default function Hero() {
               </div>
             </div>
             {/* Picture */}
-            <div className="border border-red-400 lg:h-full w-full h-[65%]">
-              Picture
+            <div className="relative  p-1 z-0 flex items-center overflow-hidden animate-warp justify-center transition-all max-w-[600px] w-[80%] sm:w-[30%] md:w-[80%] mx-auto h-fit">
+              <div className=" bg-gradient-to-bl absolute -z-[1] from-blue-100 to-blue-700 w-[600px] h-[600px] animate-slowSpin"></div>
+              <Image
+                src={heroPicture}
+                alt="A close-up of myself"
+                className="animate-warp shadow-2xl transition-all"
+              />
             </div>
           </div>
           {/* Tech Stack */}
-          <div className=" h-fit gap-8 mb-32 lg:mb-8 flex md:flex-row flex-col items-center justify-center md:justify-start w-full">
+          <div className=" h-fit gap-8 mt-8 mb-16 lg:mb-8 flex md:flex-row flex-col items-center justify-center md:justify-start w-full">
             <div className=" w-fit md:w-fit text-center md:text-left font-semibold  border-b-2 border-black md:border-none text-lg opacity-80 md:after:content-['_|']">
               My Tech Stack
             </div>
