bf05aec9090317de3eaf68aead93c89345daf40c
Luke Cloete <Cloeteluke@gmail.com>
2023-10-29 22:51:46 +0200

    Add more tags to Head component

--- portfolio-website/src/pages/index.tsx
+++ portfolio-website/src/pages/index.tsx
@@ -11,6 +11,27 @@ export default function Home() {
     <>
       <Head>
         <title>Luke Cloete | Front-end Developer</title>
+        <link
+          rel="apple-touch-icon"
+          sizes="180x180"
+          href="/apple-touch-icon.png"
+        />
+
+        <meta
+          name="description"
+          content="Welcome to my portfolio! Explore my collection of my projects showcasing my expertise and get in touch.
+            With a passion for clean code and innovative solutions, I strive to create seamless user experiences that blend functionality with stunning designs."
+        />
+        <meta
+          name="keywords"
+          content="Namibian Developer, Front-end developer, software developer, software engineer, Luke Cloete, Web developer, Namibian web developer"
+        />
+        <meta name="author" content="Luke Cloete, cloeteluke@gmail.com" />
+        <meta
+          name="My personal portfolio site."
+          content="Front-end developer portfolio"
+        />
+        <link rel="manifest" href="/site.webmanifest" />
       </Head>
 
       <main>
